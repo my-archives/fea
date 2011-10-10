15aa8d0a643bfc170ebdffc4929082dbec66d9f4
cfddream <cfddream@gmail.com>
2011-10-10 11:02:52 +0800

    updated test

---
+++ test/js/pagelet-1.js
@@ -0,0 +1,2 @@
+var pagelet = document.getElementById('pagelet-1');
+console.log('Loaded pagelet-1');
---
+++ test/js/pagelet-2.js
@@ -0,0 +1,2 @@
+var pagelet = document.getElementById('pagelet-2');
+console.log('Loaded pagelet-2');
